57cd47f6e9569b9f413938c6b33ea6f2bdfa76c7
Jacob Cole <92756291+jacobcole34@users.noreply.github.com>
2023-06-01 14:24:55 -0400

    compress

--- structy-array-and-string.js
+++ structy-array-and-string.js
@@ -1,3 +1,10 @@
+// Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+
+// <number><char>
+
+// for example, '2c' or '3a'.
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
 const uncompress = (s) => {
   // todo
   
@@ -20,3 +27,38 @@ const uncompress = (s) => {
   }
   return myString;
 };
+
+// Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
+
+// 'aaa' compresses to '3a'
+// 'cc' compresses to '2c'
+// 't' should remain as 't'
+// You can assume that the input only contains alphabetic characters.
+
+const compress = (s) => {
+  // todo
+  
+  let myString = '';
+  count = 1;
+  
+  for(let i = 0; i < s.length; i++){
+    if(s[i] !== s[i+1]){
+      if(count === 1){
+        myString += s[i];
+        count = 1;
+      }
+      else {
+        myString += count + s[i];
+        count = 1;
+      }
+    }
+    else{
+      count++;
+    }
+  }
+    
+  
+  return myString;
+  
+};
+
