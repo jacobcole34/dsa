026c495227caab462ce11cde9c6daff78f9329ac
Jacob Cole <92756291+jacobcole34@users.noreply.github.com>
2023-06-08 11:41:17 -0400

    sumList

--- structy-array-and-string.js
+++ structy-array-and-string.js
@@ -218,3 +218,24 @@ const linkedListValues = (head) => {
   
   return myValues;
 };
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const sumList = (head) => {
+  // todo
+  let sum = 0;
+  let current = head;
+  
+  while(current !== null){
+    sum += current.val;
+    current = current.next;
+  }
+  
+  return sum;
+  
+};
