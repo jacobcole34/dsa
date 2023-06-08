b7ec7550e5adc42cc3425f983e9ff61e786b2932
Jacob Cole <92756291+jacobcole34@users.noreply.github.com>
2023-06-08 13:36:03 -0400

    reverseLinkedList

--- structy-array-and-string.js
+++ structy-array-and-string.js
@@ -277,3 +277,23 @@ const getNodeValue = (head, index) => {
   
   
 };
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const reverseList = (head) => {
+  // todo
+  let prev = null;
+  let current = head;
+  while(current !== null){
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
