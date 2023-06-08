1b214a856e97f135b972dd8c96778c7081e126c7
Jacob Cole <92756291+jacobcole34@users.noreply.github.com>
2023-06-08 11:44:36 -0400

    linkedListFind

--- structy-array-and-string.js
+++ structy-array-and-string.js
@@ -239,3 +239,20 @@ const sumList = (head) => {
   return sum;
   
 };
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const linkedListFind = (head, target) => {
+  // todo
+  let current = head;
+  while(current !== null){
+    if(current.val === target)return true;
+    current = current.next;
+  }  
+  return false;
+};
