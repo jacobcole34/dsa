b09b48b1d4dfe5fe832c549e6c3f15cc2fe897be
Jacob Cole <92756291+jacobcole34@users.noreply.github.com>
2023-06-08 11:49:15 -0400

    getNodeValue

--- structy-array-and-string.js
+++ structy-array-and-string.js
@@ -256,3 +256,24 @@ const linkedListFind = (head, target) => {
   }  
   return false;
 };
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const getNodeValue = (head, index) => {
+  // todo
+  let count = 0;
+  let current = head;
+  while(current !== null){
+    if(count === index)return current.val;
+    count++;
+    current = current.next;
+  }
+  return null;
+  
+  
+};
